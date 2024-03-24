acfa6cebea7548a41e24a74d9759528a6f1b87e2
Dieny Simon <simon.dieny@gmail.com>
2024-03-24 12:36:05 +0100

    step 1 - implement reactive forcast list page

--- src/app/forecasts-list/forecasts-list.component.ts
+++ src/app/forecasts-list/forecasts-list.component.ts
@@ -1,23 +1,26 @@
-import { Component } from '@angular/core';
-import {WeatherService} from '../weather.service';
-import {ActivatedRoute} from '@angular/router';
-import {Forecast} from './forecast.type';
+import { Component, OnInit, inject } from "@angular/core";
+import { WeatherService } from "../weather.service";
+import { ActivatedRoute, ParamMap, Params } from "@angular/router";
+import { Forecast } from "./forecast.type";
+import { filter, map, switchMap } from "rxjs/operators";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-forecasts-list',
-  templateUrl: './forecasts-list.component.html',
-  styleUrls: ['./forecasts-list.component.css']
+  selector: "app-forecasts-list",
+  templateUrl: "./forecasts-list.component.html",
+  styleUrls: ["./forecasts-list.component.css"],
 })
-export class ForecastsListComponent {
+export class ForecastsListComponent implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly weatherService = inject(WeatherService);
+  forecast$: Observable<Forecast>;
 
-  zipcode: string;
-  forecast: Forecast;
-
-  constructor(protected weatherService: WeatherService, route : ActivatedRoute) {
-    route.params.subscribe(params => {
-      this.zipcode = params['zipcode'];
-      weatherService.getForecast(this.zipcode)
-        .subscribe(data => this.forecast = data);
-    });
+  /* Use reactive implementation to avoid handling unsubscription. */
+  ngOnInit(): void {
+    this.forecast$ = this.route.paramMap.pipe(
+      filter((params: ParamMap) => params.has("zipcode")),
+      map((params: ParamMap) => params.get("zipcode")),
+      switchMap((zipcode: string) => this.weatherService.getForecast(zipcode))
+    );
   }
 }
