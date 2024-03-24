53a18a752e43128d7af9d202e18397e491487e2e
Dieny Simon <simon.dieny@gmail.com>
2024-03-24 13:04:28 +0100

    step 1 - weather service as a simple http caller

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Signal, signal } from "@angular/core";
+import { Injectable, Signal, inject, signal } from "@angular/core";
 import { Observable } from "rxjs";
 
 import { HttpClient } from "@angular/common/http";
@@ -13,33 +13,9 @@ export class WeatherService {
   static APPID = "5a4b2d457ecbef9eb2a71e480b947604";
   static ICON_URL =
     "https://raw.githubusercontent.com/udacity/Sunshine-Version-2/sunshine_master/app/src/main/res/drawable-hdpi/";
-  private currentConditions = signal<ConditionsAndZip[]>([]);
+  private readonly http = inject(HttpClient);
 
-  constructor(private http: HttpClient) {}
-
-  addCurrentConditions(zipcode: string): void {
-    // Here we make a request to get the current conditions data from the API. Note the use of backticks and an expression to insert the zipcode
-    this.http
-      .get<CurrentConditions>(
-        `${WeatherService.URL}/weather?zip=${zipcode},us&units=imperial&APPID=${WeatherService.APPID}`
-      )
-      .subscribe((data) =>
-        this.currentConditions.update((conditions) => [
-          ...conditions,
-          { zip: zipcode, data },
-        ])
-      );
-  }
-
-  removeCurrentConditions(zipcode: string) {
-    this.currentConditions.update((conditions) => {
-      for (let i in conditions) {
-        if (conditions[i].zip == zipcode) conditions.splice(+i, 1);
-      }
-      return conditions;
-    });
-  }
-
+  // KEEP
   fetchConditionsByLocation(zipcode: string): Observable<CurrentConditions> {
     const baseUrl = `${WeatherService.URL}/weather`;
     const queryParams = `?zip=${zipcode},us&units=imperial&APPID=${WeatherService.APPID}`;
@@ -47,11 +23,7 @@ export class WeatherService {
     return this.http.get<CurrentConditions>(`${baseUrl}${queryParams}`);
   }
 
-  /** @deprecated */
-  getCurrentConditions(): Signal<ConditionsAndZip[]> {
-    return this.currentConditions.asReadonly();
-  }
-
+  // KEEP
   getForecast(zipcode: string): Observable<Forecast> {
     // Here we make a request to get the forecast data from the API. Note the use of backticks and an expression to insert the zipcode
     return this.http.get<Forecast>(
@@ -59,6 +31,7 @@ export class WeatherService {
     );
   }
 
+  // KEEP
   getWeatherIcon(id): string {
     if (id >= 200 && id <= 232)
       return WeatherService.ICON_URL + "art_storm.png";
