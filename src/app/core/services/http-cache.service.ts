109ee885aad4c5d52d46eaf1c07ca386387ea536
Dieny Simon <simon.dieny@gmail.com>
2024-03-27 13:17:57 +0100

    step 3 - renaming in http interceptor

--- src/app/core/services/http-cache.service.ts
+++ src/app/core/services/http-cache.service.ts
@@ -1,39 +1,44 @@
+import { HttpResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 type HttpRequestCached = {
-  value: unknown;
+  value: HttpResponse<unknown>;
   expiration: number;
 };
 @Injectable({
   providedIn: "root",
 })
 export class HttpCacheService {
-  save(key: string, data: any, expirationInMinutes: number): void {
-    const record: HttpRequestCached = {
-      value: data,
+  save(
+    key: string,
+    httpResponse: HttpResponse<unknown>,
+    expirationInMinutes: number
+  ): void {
+    const httpResponseCached: HttpRequestCached = {
+      value: httpResponse,
       expiration: new Date().getTime() + expirationInMinutes * 60000,
     };
-    localStorage.setItem(key, JSON.stringify(record));
+    localStorage.setItem(key, JSON.stringify(httpResponseCached));
   }
 
   load(key: string): any | null {
-    const item = localStorage.getItem(key);
+    const httpResponseCachedAsString = localStorage.getItem(key);
 
     /* No http request was found in cache. */
-    if (item === null) {
+    if (httpResponseCachedAsString === null) {
       return null;
     }
 
     /* Http request was found in cache but has timed out. */
-    const record = JSON.parse(item);
+    const response: HttpRequestCached = JSON.parse(httpResponseCachedAsString);
     const now = new Date().getTime();
-    const isHttpRequestHasExpired = now > record.expiration;
+    const isHttpRequestHasExpired = now > response.expiration;
 
     if (isHttpRequestHasExpired) {
       localStorage.removeItem(key);
       return null;
     }
 
-    return record.value;
+    return response.value;
   }
 }
