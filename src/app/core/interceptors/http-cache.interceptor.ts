294be9f6a6d6dd5f53f9386b6a1d1cf47a02d5c9
Dieny Simon <simon.dieny@gmail.com>
2024-03-27 13:29:28 +0100

    step 3 - enforce type and add comment on interceptor

--- src/app/core/interceptors/http-cache.interceptor.ts
+++ src/app/core/interceptors/http-cache.interceptor.ts
@@ -12,48 +12,47 @@ import { tap } from "rxjs/operators";
 
 @Injectable()
 export class HttpCacheInterceptor implements HttpInterceptor {
-  httpCacheService = inject(HttpCacheService);
+  private readonly httpCacheService = inject(HttpCacheService);
+  private static readonly EXPIRATION_TIME_IN_MINUTES = 120;
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    if (!this.isCachable(request)) {
+    if (!this.isCacheable(request)) {
       return next.handle(request);
     }
 
     const key = this.getCacheKey(request);
-    const record = this.httpCacheService.load(key);
+    const response: HttpResponse<unknown> | null =
+      this.httpCacheService.load(key);
 
-    // record == httpResponse
-    if (record) {
-      console.log("We use cached response !");
-      console.log(record);
-      const res = new HttpResponse({ body: record.body, status: 200 });
-      return of(res);
+    if (response) {
+      console.info(`We use cached response for request ${key}`);
+      return of(new HttpResponse({ body: response.body, status: 200 }));
     }
 
-    if (!record) {
-      console.log("We send a request to the backend");
-      return next.handle(request).pipe(
-        tap((event) => {
-          if (event instanceof HttpResponse) {
-            const key = this.getCacheKey(request);
-            const twoHoursInMinutes = 120;
-            console.log("EVENT BODY");
-            console.log(event.body);
-            this.httpCacheService.save(key, event, twoHoursInMinutes);
-          }
-        })
-      );
-    }
+    return next.handle(request).pipe(
+      tap((event) => {
+        if (event instanceof HttpResponse) {
+          console.log(`We send to the backend the request ${key}`);
+          this.httpCacheService.save(
+            key,
+            event,
+            HttpCacheInterceptor.EXPIRATION_TIME_IN_MINUTES
+          );
+        }
+      })
+    );
   }
 
+  /* Determine unique key for a given request. */
   private getCacheKey(request: HttpRequest<any>): string {
     return request.urlWithParams;
   }
 
-  private isCachable(request: HttpRequest<unknown>) {
+  /* Determine if a given request is cachable. */
+  private isCacheable(request: HttpRequest<unknown>): boolean {
     const isRequestGET = request.method === "GET";
     const isWeatherInfoRequest = request.url.startsWith(
       "http://api.openweathermap.org/data/2.5/weather?"
